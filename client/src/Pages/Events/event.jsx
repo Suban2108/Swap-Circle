e578557af3bbe4847b2511ce0af154cd1be10b82
Abdul Suban <122554707+Suban2108@users.noreply.github.com>
2025-07-06 15:33:50 +0530

    Debugged error no. 2

--- client/src/Pages/Events/event.jsx
+++ client/src/Pages/Events/event.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/Components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { EventCard } from "./Component/EventCard"
 import { EventFilters } from "./Component/EventFilter"
